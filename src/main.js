449e090afa05f8a0080303add2920ed8c207b0cc
Evangelia Ioannou <evievi@outlook.com>
2021-05-14 13:16:40 +0200

    Included Bootstrap-Vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
