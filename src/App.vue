8c91da64bebf709c13e00f09a8d058c6e5f30ca8
Evangelia Ioannou <evievi@outlook.com>
2021-05-21 11:25:33 +0200

    Navbar link styling #6

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,59 @@
 <template>
   <div id="app">
-      <b-navbar toggleable="md">
-          <b-navbar-brand href="#">
-              <b-img 
-              class="logo" 
-              src="https://firebasestorage.googleapis.com/v0/b/coffeeverse-by-evi.appspot.com/o/Website%20images%2FCoffeeVerseLogo.png?alt=media&token=f43f1ff9-b409-4a4a-b9af-ef2eb184ba1b"
-              alt="coffeeverse-logo"
-              >
-              </b-img>
-          </b-navbar-brand>
-          <b-navbar-toggle target="nav-collapse" class="ml-auto"></b-navbar-toggle>
-          <!-- Show the cart nav-item HERE when screen is smaller than 768px -->
-          <b-navbar-nav class="d-md-none">
-              <b-nav-item class="mr-2 ml-3" href="#">Cart</b-nav-item>
-          </b-navbar-nav>
-          <b-collapse id="nav-collapse" is-nav>
-              <b-navbar-nav class="ml-md-auto m-0">
-                <b-nav-item class="mr-3" href="#">Home</b-nav-item>
-                <b-nav-item class="mr-3" href="#">Drinks</b-nav-item>
-                <b-nav-item class="mr-3" href="#">Snacks</b-nav-item>
-                <b-nav-item class="mr-3" href="#">Order status</b-nav-item>         
-              </b-navbar-nav>
-          </b-collapse>
-          <!-- Show the cart nav-item HERE when screen is larger than 768px -->
-          <b-navbar-nav class="d-none d-md-block">
-              <b-nav-item class="mr-3" href="#">Cart</b-nav-item>
-          </b-navbar-nav>
-      </b-navbar>
+    <b-navbar toggleable="md">
+      <b-navbar-brand href="#">
+        <b-img
+          class="logo"
+          src="https://firebasestorage.googleapis.com/v0/b/coffeeverse-by-evi.appspot.com/o/Website%20images%2FCoffeeVerseLogo.png?alt=media&token=f43f1ff9-b409-4a4a-b9af-ef2eb184ba1b"
+          alt="coffeeverse-logo"
+        >
+        </b-img>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" class="ml-auto"></b-navbar-toggle>
+      <!-- Show the cart nav-item HERE when screen is smaller than 768px -->
+      <b-navbar-nav class="d-md-none">
+        <b-nav-item class="mr-2 ml-3" href="#">Cart</b-nav-item>
+      </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-md-auto m-0">
+          <b-nav-item class="mr-3" href="#" active>Home</b-nav-item>
+          <b-nav-item class="mr-3" href="#">Drinks</b-nav-item>
+          <b-nav-item class="mr-3" href="#">Snacks</b-nav-item>
+          <b-nav-item class="mr-3" href="#">Order status</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+      <!-- Show the cart nav-item HERE when screen is larger than 768px -->
+      <b-navbar-nav class="d-none d-md-block">
+        <b-nav-item class="mr-3" href="#">Cart</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <router-view />
   </div>
 </template>
 
 <style>
-    .logo {
-        height: 40px;
+  * {
+    font-family: 'Montserrat', sans-serif;
+  }
+  .navbar-light .navbar-nav .nav-link {
+    color: #17433e;
+  }
+
+  .navbar-light .navbar-nav .active {
+    color: #17433e !important;
+    border-bottom: solid 1px #276f65;
+  }
+  .logo {
+    height: 40px;
+  }
+  p {
+    font-size: 14px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    .navbar-light .navbar-nav .active {
+      border: none;
+      font-weight: bold;
     }
-    p {
-        font-size: 14px;
-    }
+  }
 </style>
--- src/components/Home/HeroImage.vue
+++ src/components/Home/HeroImage.vue
@@ -1,33 +1,32 @@
 <template>
-    <div>
-        <b-jumbotron 
-            fluid
-            header="Coffeeverse" 
-            id="heroImage" 
-            lead="City's best fika right at your door" 
-            >
-            <b-button href="#order">Order now</b-button>
-        </b-jumbotron>
-    </div>
-
+  <div>
+    <b-jumbotron
+      fluid
+      header="Coffeeverse"
+      id="heroImage"
+      lead="City's best fika right at your door"
+    >
+      <b-button href="#order">Order now</b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style scoped>
-    #heroImage {
-        background-image: url("https://firebasestorage.googleapis.com/v0/b/coffeeverse-by-evi.appspot.com/o/Website%20images%2Fbackground-coffee.jpg?alt=media&token=a83259bc-9826-4d83-8027-931557ba3f1c");
-        background-size: cover;
-        background-position: 90% 60%;
-        color: #0D2522;
-    }
-
-    #heroImage .btn {
-        background-color: #276F65
-    }
-
-
+  #heroImage {
+    background-image: url('https://firebasestorage.googleapis.com/v0/b/coffeeverse-by-evi.appspot.com/o/Website%20images%2Fbackground-coffee.jpg?alt=media&token=a83259bc-9826-4d83-8027-931557ba3f1c');
+    background-size: cover;
+    background-position: 90% 60%;
+    color: #0d2522;
+  }
+  #heroImage * {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      'Helvetica Neue', Arial, 'Noto Sans', 'Liberation Sans', sans-serif,
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+      'Noto Color Emoji';
+  }
+  #heroImage .btn {
+    background-color: #276f65;
+  }
 </style>
-
