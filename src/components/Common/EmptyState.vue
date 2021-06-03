72dc1aabd9a43e2f8b404a3be0ed190518b4dbdd
Evangelia Ioannou <evievi@outlook.com>
2021-06-03 11:05:34 +0200

    Create common component for empty state #32

---
+++ src/components/Common/EmptyState.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col class="content"
+        ><b-img class="illustration" :src="img"></b-img>
+        <p class="h4">{{ message }}</p>
+        <router-link to="/" class="homeLink">{{ linkText }}</router-link></b-col
+      >
+    </b-row>
+  </b-container>
+</template>
+<script>
+  export default {
+    props: ['img', 'message', 'linkText']
+  }
+</script>
+<style scoped>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .homeLink {
+    color: #39a88b;
+  }
+  .illustration {
+    height: 70vh;
+  }
+</style>
--- src/views/OrderStatus.vue
+++ src/views/OrderStatus.vue
@@ -3,7 +3,12 @@
     <b-row>
       <b-col class="d-flex flex-column align-items-center">
         <h1 class="h2">Order Status</h1>
-        <empty-orders v-if="$store.state.orders.length == 0" />
+        <empty-state
+          v-if="$store.state.orders.length == 0"
+          img="https://firebasestorage.googleapis.com/v0/b/coffeeverse-by-evi.appspot.com/o/Website%20images%2Fclip-shopping-online.png?alt=media&token=708262b5-23d6-4f51-bd6a-4bb45af37662"
+          message="You have no ongoing orders"
+          linkText="Place a new order"
+        />
         <full-orders v-else />
       </b-col>
     </b-row>
@@ -11,10 +16,10 @@
 </template>
 
 <script>
-  import EmptyOrders from '../components/Orders/EmptyOrders.vue'
+  import EmptyState from '../components/Common/EmptyState.vue'
   import FullOrders from '../components/Orders/FullOrders.vue'
   export default {
-    components: { EmptyOrders, FullOrders }
+    components: { EmptyState, FullOrders }
   }
 </script>
 
